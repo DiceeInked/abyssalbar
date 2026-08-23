04807ef7400454e695eeac6ae3b399496184069a
DiceeInked <brucee@pillers.us>
2026-08-22 22:25:30 -0500

    Add Etho page

---
+++ app/etho/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Etho() {
+  return (
+    <main className="terminal">
+      <div className="scanlines" />
+
+      <div className="terminal-container">
+        <section className="terminal-window">
+          <div className="terminal-title">ETHO</div>
+
+          <div className="terminal-output">
+            <p>Etho page</p>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
