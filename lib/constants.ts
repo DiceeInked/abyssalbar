5756c0cf3a55ba6de27691ab2fc859cbc75754bd
DiceeInked <brucee@pillers.us>
2026-08-24 19:28:51 -0500

    Centralize Abyssal Bar shared constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
+export const SITE_VERSION = "2.22";
+
 export const SESSION_COOKIE = "abyssal_session";
 
 export const USERNAME_MIN = 3;
@@ -6,4 +8,9 @@ export const PASSWORD_MIN = 3;
 export const PASSWORD_MAX = 30;
 
 export const GAME_BUCKET = "gametonics";
+export const GAME_LIBRARY_ROUTE = "/gametonics";
+
 export const MAX_MESSAGES = 64;
+export const MESSAGE_LINE_LENGTH = 80;
+
+export const ETHO_TRANSITION_MS = 900;
